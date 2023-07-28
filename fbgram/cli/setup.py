c7bf19504432a96836bdd23a98d052e7e22f7317
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2023-07-28 08:12:32 +0300

    feat: setup_telegram_bot()

--- fbgram/cli/setup.py
+++ fbgram/cli/setup.py
@@ -1,5 +1,73 @@
-def setup_telegram_bot():
-    print("Setting up telegram bot")
+from random import randint
+from time import sleep
+
+import rich
+import typer
+from db.models import Settings
+from rich.prompt import Prompt
+from telegram.telegram import TelegramBot
+
+MAX_SETUP_TELEGRAM_BOT_ATTEMPTS = 3
+
+
+###############################################
+# Telegram bot setup
+###############################################
+def is_telegram_bot_set_up():
+    settings = Settings.load()
+    return (
+        settings.telegram_bot_token is not None
+        and settings.telegram_chat_id is not None
+    )
+
+
+def setup_telegram_bot() -> None:
+    if is_telegram_bot_set_up():
+        rich.print("Telegram bot is set up")
+        return
+    rich.print("You have to set up a telegram bot to use fbgram.")
+    rich.print("To do so, follow these steps:")
+    rich.print("1. Open Telegram and search for [bold]@BotFather[/bold]")
+    rich.print("2. Send [bold]/newbot[/bold] command to BotFather")
+    rich.print("3. Follow the instructions to create a new bot")
+    rich.print("4. Copy the bot token and paste it here:")
+
+    attempts = MAX_SETUP_TELEGRAM_BOT_ATTEMPTS
+    while True:
+        token = Prompt.ask("Bot token")
+        try:
+            updates = TelegramBot.get_updates(token=token)
+        except Exception as e:
+            rich.print(f"[red]Error: {e}[/red]")
+            attempts -= 1
+            if attempts == 0:
+                rich.print(
+                    f"You failed to set up the telegram bot {MAX_SETUP_TELEGRAM_BOT_ATTEMPTS} times."
+                )
+                raise typer.Abort()
+            rich.print('Try again or press "Ctrl+C" to exit.')
+            continue
+        break
+
+    confirm_number = str(randint(100000, 999999))
+    rich.print(
+        "Now you have to confirm that you are the owner of this bot. To do this, follow these steps:"
+    )
+    rich.print("1. Open Telegram and search for your bot")
+    rich.print("2. Send [bold]/start[/bold] command to your bot")
+    rich.print(f"3. Enter the following number: [bold]{confirm_number}[/bold]")
+    while not updates or updates[-1]["message"]["text"] != confirm_number:
+        sleep(1)
+        updates = TelegramBot.get_updates(token=token)
+
+    chat_id = updates[-1]["message"]["chat"]["id"]
+    settings = Settings.load()
+    settings.telegram_bot_token = token
+    settings.telegram_chat_id = chat_id
+    settings.save()
+    bot = TelegramBot(token=token, chat_id=chat_id)
+    bot.send_test_message()
+    rich.print("[green]Telegram bot set up successfully[/green]]\n")
 
 
 def setup_browser():
