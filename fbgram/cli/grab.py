58797f47793c2185471c6aa2632385829ae4d34f
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2023-07-28 15:33:19 +0300

    feat: grab()

--- fbgram/cli/grab.py
+++ fbgram/cli/grab.py
@@ -1,2 +1,26 @@
+import rich
+import typer
+from browser.browser import FBBrowser
+from services.grabber import grab_new_posts
+
+
 def grab():
-    print("Grabbing...")
+    if not FBBrowser.is_installed():
+        rich.print(
+            f"[red]Browser is not installed. Please run `fbgram setup` first.[/red]"
+        )
+        raise typer.Abort()
+
+    with FBBrowser() as browser:
+        if not browser.is_logged_in():
+            rich.print(
+                f"[red]You are not logged in to Facebook. Please run `fbgram setup` first.[/red]"
+            )
+            raise typer.Abort()
+        rich.print("Grabbing new posts from Facebook...")
+        new_posts_count = grab_new_posts(browser)
+        if new_posts_count == 0:
+            rich.print("No new posts found.")
+            return
+    rich.print(f"Grabbed {new_posts_count} new posts from Facebook.")
+    return
