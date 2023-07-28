6e3d6fecf38f911b6b28374efc2e4fbaafc44dc3
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2023-07-28 06:57:09 +0300

    feat: add telegram bot class

---
+++ fbgram/telegram/telegram.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from typing import Literal
+
+from notifiers import get_notifier
+
+
+class TelegramBot:
+    notifier = get_notifier("telegram")
+
+    def __init__(self, token: str, chat_id: str) -> None:
+        self.token = token
+        self.chat_id = chat_id
+
+    def send_message(
+        self,
+        message: str,
+        parse_mode: Literal["html", "markdown"] = "markdown",
+        disable_web_page_preview: bool = True,
+        disable_notification: bool = True,
+    ) -> None:
+        self.notifier.notify(
+            token=self.token,
+            chat_id=self.chat_id,
+            message=message,
+            parse_mode=parse_mode,
+            disable_web_page_preview=disable_web_page_preview,
+            disable_notification=disable_notification,
+        )
+
+    def send_test_message(self) -> None:
+        current_datetime_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f"Test message from fbgram at {current_datetime_str}"
+        self.send_message(message=message, disable_notification=False)
+
+    @classmethod
+    def get_updates(cls, token: str) -> list:
+        updates = cls.notifier.get_updates(token=token)
+        return updates
